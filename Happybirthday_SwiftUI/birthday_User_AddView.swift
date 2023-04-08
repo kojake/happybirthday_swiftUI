71f0131621ac3a6d0f36a58130c2ae584c39e343
kaito <takahashikaito@kaitonoMacBook-Air.local>
2023-04-08 20:35:52 +0900

    update

--- Happybirthday_SwiftUI/birthday_User_AddView.swift
+++ Happybirthday_SwiftUI/birthday_User_AddView.swift
@@ -11,6 +11,8 @@ struct birthday_User_AddView: View {
 
     @State private var image: UIImage? = nil
     @State private var showingImagePicker = false
+    
+    //画面を閉じる
     @Environment(\.presentationMode) var presentationMode
     
     //生年月日
@@ -23,14 +25,17 @@ struct birthday_User_AddView: View {
     //error_alert
     @State private var ShouldShowerror_alert = false
     
+    //文字数制限
+    private let maxPasswordLength = 4
+    
     var body: some View {
         NavigationView {
             Form {
                 Text("誕生日の人の名前と生年月日を入力して下さい").font(.title2).fontWeight(.black)
                 TextField("タップして名前を入力", text: $name).font(.title2).fontWeight(.black)
-                TextField("タップして年を入力", text: $year).font(.title2).fontWeight(.black)
-                TextField("タップして月を入力", text: $month).font(.title2).fontWeight(.black)
-                TextField("タップして日を入力", text: $day).font(.title2).fontWeight(.black)
+                TextField("タップして年を入力", text: $year).font(.title2).fontWeight(.black).keyboardType(.numberPad)
+                TextField("タップして月を入力", text: $month).font(.title2).fontWeight(.black).keyboardType(.numberPad)
+                TextField("タップして日を入力", text: $day).font(.title2).fontWeight(.black).keyboardType(.numberPad)
 
                 if let image = image {
                     Image(uiImage: image)
@@ -43,23 +48,26 @@ struct birthday_User_AddView: View {
                     Text("写真を選択する")
                 }
                 HStack{
-                    Text("追加する場合は右上にある").fontWeight(.black)
+                    Text("生年月日と画像を挿入できたら").fontWeight(.black)
                     Text("追加").foregroundColor(.blue)
                     Text("をタップして下さい").fontWeight(.black)
                 }
                 
                 .alert(isPresented: $ShouldShowerror_alert) {
                     Alert(title: Text("エラー"),
-                          message: Text("どこかの欄が入力または画像が挿入されていません"))
+                          message: Text("生年月日のどれかの欄が入力されていません又は、年が1926年から2023の間で入力されていない"))
                 }
-                
             }
             .navigationBarTitle("誕生日人を追加する")
+            .navigationBarItems(leading: Button("✖️"){
+                presentationMode.wrappedValue.dismiss()
+            })
             .navigationBarItems(trailing: Button("追加") {
-                
                 //西暦を保存する
+                //生年月日が入力されているかを判断する
                 if (name == "") || (year == "") || (month == "") || (day == ""){
-                    
+                    //生年月日が入力されていないなら、erroralertを表示する
+                    ShouldShowerror_alert = true
                 }
                 else{
                     if (Int(year)! >= 1926) && (Int(year)! <= 1989){
@@ -70,6 +78,9 @@ struct birthday_User_AddView: View {
                     }
                     else if (Int(year)! >= 2020) && (Int(year)! <= 2023){
                         japanese_calender = "令和"
+                    }
+                    else{
+                        ShouldShowerror_alert = true
                     }
                 }
                 
--- Happybirthday_SwiftUI/mainView.swift
+++ Happybirthday_SwiftUI/mainView.swift
@@ -23,34 +23,36 @@ struct MainView: View {
     
     //画面遷移
     @State private var showPhotoAddView = false
-    @State private var showsettingView = false
+    @State private var showstrashView = false
     
     var body: some View {
         NavigationView {
             List {
                 ForEach(Birthday_User) { item in
-                    VStack {
+                    VStack{
                         HStack{
-                            Text("名前：").fontWeight(.black).font(.title)
+                            Text("名前").fontWeight(.black).font(.title)
                             Text(item.name).fontWeight(.black).font(.largeTitle)
                             Spacer()
+                            Text("西暦").fontWeight(.black).font(.title)
+                            Text(item.japanese_calender).fontWeight(.black).font(.largeTitle)
                         }
                         HStack{
                             Image(uiImage: item.image)
                                 .resizable()
                                 .frame(width: 100, height: 100)
                                 .clipShape(Circle())
-                            Text(item.year).fontWeight(.black).font(.largeTitle)
+                            Text(item.year).fontWeight(.black).font(.title)
                             Text("/")
-                            Text(item.month).fontWeight(.black).font(.largeTitle)
+                            Text(item.month).fontWeight(.black).font(.title)
                             Text("/")
-                            Text(item.day).fontWeight(.black).font(.largeTitle)
+                            Text(item.day).fontWeight(.black).font(.title)
                             Spacer()
 
                         }
                     }
                 }
-            }//.onDelete(perform: rowRemove)
+            }
             
             .navigationBarTitle("HappyBirthday")
             .navigationBarItems(trailing: Button(action: {
@@ -62,151 +64,14 @@ struct MainView: View {
                 birthday_User_AddView(Birthday_User: $Birthday_User)
             }
             .navigationBarItems(leading: Button(action: {
-                showPhotoAddView.toggle()
+
             }) {
-                Image(systemName: "gearshape.fill").padding().background(Color.brown).foregroundColor(.white).clipShape(Circle()).padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
+                Image(systemName: "trash.fill").padding().background(Color.brown).foregroundColor(.white).clipShape(Circle()).padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
             })
-            .sheet(isPresented: $showsettingView) {
-                setting()
-            }
         }
     }
-    
-//    func rowRemove(offsets: IndexSet) {
-//        birthday_User.remove(atOffsets: offsets)
-//    }
 }
 
-//struct mainView: View {
-//    @State private var showPhotoAddView = false
-//
-//    @State var birthday_list = [String]()
-////    UserDefaults.standard.object(forKey: "birthday_list_key") as! [String]
-//
-//    //errorlabel
-//    @State var error_text = ""
-//
-//    //生年月日item
-//    @State var year = ""
-//    @State var month = ""
-//    @State var day = ""
-//    @State var name = ""
-//
-//    //photo
-//    @State var photos = ""
-//
-//    //add_birthday_ok_alert
-//    @State private var add_birthday_ok = false
-//
-//    var body: some View {
-//        NavigationView {
-//                List {
-//                    ForEach(photos) { photo in
-//                        HStack {
-//                            Text(photo.name)
-//                            Spacer()
-//                            Image(uiImage: photo.image)
-//                                .resizable()
-//                                .frame(width: 50, height: 50)
-//                        }
-//                    }
-//                }
-//                .navigationBarTitle("Photos")
-//                .navigationBarItems(trailing: Button(action: {
-//                    showPhotoAddView.toggle()
-//                }) {
-//                    Image(systemName: "plus")
-//                })
-//                .sheet(isPresented: $showPhotoAddView) {
-//                    PhotoAddView(photos: $photos)
-//                }
-//
-//                List{
-//                    HStack{
-//                        Spacer()
-//                        Text("誕生日を覚える人を追加する").font(.title2).fontWeight(.black)
-//                        Spacer()
-//                    }
-//                    VStack{
-//                        Text("1.名前を入力してください").font(.title2).fontWeight(.black)
-//                    }
-//                    HStack{
-//                        TextField("自分の名前を入力してください",text: $name)
-//                            .textFieldStyle(RoundedBorderTextFieldStyle())
-//                            .padding()
-//                    }
-//                    VStack{
-//                        Text("2.生年月日を入力してください").font(.title2).fontWeight(.black)
-//                    }
-//                    HStack{
-//                        TextField("生まれた年を入力してください",text: $year).keyboardType(.numberPad)
-//                            .textFieldStyle(RoundedBorderTextFieldStyle())
-//                            .padding()
-//                    }
-//                    HStack{
-//                        TextField("生まれた月を入力してください",text: $month).keyboardType(.numberPad)
-//                            .textFieldStyle(RoundedBorderTextFieldStyle())
-//                            .padding()
-//                    }
-//                    HStack{
-//                        TextField("生まれた日を入力してください",text: $day).keyboardType(.numberPad)
-//                            .textFieldStyle(RoundedBorderTextFieldStyle())
-//                            .padding()
-//                    }
-//                    HStack{
-//                        Spacer()
-//                        Spacer()
-//                    }
-//                    VStack{
-//                        Text(error_text).font(.title3).fontWeight(.black)
-//                    }
-//                }
-//
-//            }
-//            .toolbar {
-//                ToolbarItem(placement: .keyboard) {
-//                    Button("完了") {
-//                        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-//                    }
-//                }
-//            }
-//            //追加しますかの確認alert
-//            .alert(isPresented: $add_birthday_ok) {
-//                        Alert(title: Text("確認"),
-//                              message: Text("\(name):\(japanese_calender)/：\(year)/\(month)/\(day)/で生年月日を追加しますか？"),primaryButton: .cancel(Text("キャンセル")),
-//                              secondaryButton: .default(Text("追加"),
-//                                                      action: {
-//
-//                            //誕生日を保存
-//                            birthday_list.append(birthday_list_house)
-//                            //データ保存
-//                            UserDefaults.standard.set(birthday_list, forKey: "birthday_list_key")
-//                        }))
-//                    }
-//        }
-//    }
-//    func add_birthday_list(){
-//        if (name == "") || (year == "") || (month == "") || (day == ""){
-//            error_text = "どこかの欄が入力されていません"
-//        }
-//        else{
-//            if (Int(year)! >= 1926) && (Int(year)! <= 1989){
-//                japanese_calender = "昭和"
-//            }
-//            else if (Int(year)! >= 1990) && (Int(year)! <= 2019){
-//                japanese_calender = "平成"
-//            }
-//            else if (Int(year)! >= 2020) && (Int(year)! <= 2023){
-//                japanese_calender = "令和"
-//            }
-//
-//            //詳細
-//            error_text = ""
-//            birthday_list_house = ("\(name):\(japanese_calender)/：\(year)/\(month)/\(day)")
-//            add_birthday_ok = true
-//        }
-//    }
-//
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         MainView()
