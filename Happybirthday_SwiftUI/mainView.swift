f360df775d06c160071aad68c7655ae439928189
kaito <takahashikaito@takahashikaito23.local>
2023-04-25 17:07:46 +0900

    update

--- Happybirthday_SwiftUI/mainView.swift
+++ Happybirthday_SwiftUI/mainView.swift
@@ -25,6 +25,9 @@ struct MainView: View {
     @State private var showstrashView = false
     
     @State var backgroundColor_count = 0
+    @State var background_change_button_image = "sun.max.fill"
+    @State private var backgroundColor = Color.orange
+    @State private var backgroundColor2 = Color.white
     
     init(){
         UITableView.appearance().backgroundColor = UIColor.white
@@ -32,80 +35,75 @@ struct MainView: View {
     
     var body: some View {
         NavigationView {
-            List {
-                ForEach(Birthday_User) { item in
-                    NavigationLink(destination: detail_View(birthday_user_information: item)){
-                        VStack{
-                            HStack{
-                                Text("名前").fontWeight(.black).font(.title)
-                                Text(item.name).fontWeight(.black).font(.title2)
-                                Spacer()
-                                Text("西暦").fontWeight(.black).font(.title)
-                                Text(item.japanese_calender).fontWeight(.black).font(.largeTitle)
-                            }
-                            HStack{
-                                if let image = image {
-                                    Image(uiImage: image)
-                                        .resizable()
-                                        .frame(width: 100, height: 100)
-                                        .clipShape(Circle())
-                                } else {
-                                    Image("NoImage").resizable()
-                                        .frame(width: 100, height: 100)
-                                        .clipShape(Circle())
-                                        .overlay(Circle().stroke(Color.gray, lineWidth: 4))
-                                        .shadow(radius: 7)
+            ZStack{
+                backgroundColor2.ignoresSafeArea()
+                List {
+                    ForEach(Birthday_User) { item in
+                        NavigationLink(destination: detail_View(birthday_user_information: item)){
+                            VStack{
+                                HStack{
+                                    Text("名前").fontWeight(.black).font(.title)
+                                    Text(item.name).fontWeight(.black).font(.title2)
+                                    Spacer()
+                                    Text("西暦").fontWeight(.black).font(.title)
+                                    Text(item.japanese_calender).fontWeight(.black).font(.largeTitle)
                                 }
-                                Text(item.year).fontWeight(.black).font(.title)
-                                Text("/")
-                                Text(item.month).fontWeight(.black).font(.title)
-                                Text("/")
-                                Text(item.day).fontWeight(.black).font(.title)
-                                Spacer()
-                                
+                                HStack{
+                                    if let image = image {
+                                        Image(uiImage: image)
+                                            .resizable()
+                                            .frame(width: 100, height: 100)
+                                            .clipShape(Circle())
+                                    } else {
+                                        Image("NoImage").resizable()
+                                            .frame(width: 100, height: 100)
+                                            .clipShape(Circle())
+                                            .overlay(Circle().stroke(Color.gray, lineWidth: 4))
+                                            .shadow(radius: 7)
+                                    }
+                                    Text(item.year).fontWeight(.black).font(.title)
+                                    Text("/")
+                                    Text(item.month).fontWeight(.black).font(.title)
+                                    Text("/")
+                                    Text(item.day).fontWeight(.black).font(.title)
+                                    Spacer()
+                                    
+                                }
                             }
                         }
-                    }
-                }.onDelete(perform: { indexSet in
-                    Birthday_User.remove(at: indexSet.first!)
+                    }.onDelete(perform: { indexSet in
+                        Birthday_User.remove(at: indexSet.first!)
+                    })
+                }
+                .navigationBarTitle("HappyBirthday")
+                .navigationBarItems(trailing: Button(action: {
+                    showPhotoAddView.toggle()
+                }) {
+                    Image(systemName: "plus").padding().background(Color.brown).foregroundColor(.white).clipShape(Circle()).padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
                 })
-            }
-            .navigationBarTitle("HappyBirthday")
-            .navigationBarItems(trailing: Button(action: {
-                showPhotoAddView.toggle()
-            }) {
-                Image(systemName: "plus").padding().background(Color.brown).foregroundColor(.white).clipShape(Circle()).padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
-            })
-            .sheet(isPresented: $showPhotoAddView) {
-                birthday_User_AddView(Birthday_User: $Birthday_User)
-            }
-            .navigationBarItems(trailing: Button(action:{
-                if backgroundColor_count == 0{
-                    Image(systemName: "moon.fill")
-                    backgroundColor_count = 1
-                    background_color_change()
-                    print("0")
+                .sheet(isPresented: $showPhotoAddView) {
+                    birthday_User_AddView(Birthday_User: $Birthday_User)
                 }
-                else{
-                    Image(systemName: "sun.max.fill")
-                    backgroundColor_count = 0
-                    background_color_change()
-                    print("1")
-                }
-            }){
-                Image(systemName: "sun.max.fill").padding().background(Color.orange).foregroundColor(.white).clipShape(Circle()).padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
-            })
-            //削除ボタン
-            .navigationBarItems(trailing: EditButton().padding().background(Color.brown).foregroundColor(.white).clipShape(Circle()))        }
-    }
-    func background_color_change(){
-        if backgroundColor_count == 1{
-            UITableView.appearance().backgroundColor = UIColor.black
-            print("1")
-        }
-        else{
-            UITableView.appearance().backgroundColor = UIColor.white
-            print("0")
+                .navigationBarItems(trailing: Button(action:{
+                    if backgroundColor_count == 0{
+                        background_change_button_image = "moon.fill"
+                        backgroundColor = Color.gray
+                        backgroundColor2 = Color.black
+                        backgroundColor_count = 1
+                        
+                    }
+                    else{
+                        background_change_button_image = "sun.max.fill"
+                        backgroundColor = Color.orange
+                        backgroundColor2 = Color.white
+                        backgroundColor_count = 0
+                        
+                    }
+                }){
+                    Image(systemName: background_change_button_image).padding().background(backgroundColor).foregroundColor(.white).clipShape(Circle()).padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
+                })
+                //削除ボタン
+                .navigationBarItems(trailing: EditButton().padding().background(Color.brown).foregroundColor(.white).clipShape(Circle()))        }
         }
     }
 }
