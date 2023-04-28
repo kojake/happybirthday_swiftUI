cc9c32965febf28c465070f9563114f1b70e07fa
kaito <takahashikaito@takahashikaito23.local>
2023-04-28 15:58:49 +0900

    update

--- Happybirthday_SwiftUI/detail_View.swift
+++ Happybirthday_SwiftUI/detail_View.swift
@@ -9,43 +9,49 @@ import SwiftUI
 
 struct detail_View: View {
     @State var birthday_user_information: birthday_User
+    @Environment(\.dismiss) var dismiss
+    @Environment(\.presentationMode) var presentationMode
     @State var image = UserDefaults.standard.object(forKey: "savedImage") as? UIImage
-
+    
     var body: some View {
-        VStack{
-            ZStack {
-                RoundedRectangle(cornerRadius: 100)
-                    .fill(Color.gray)
-                    .shadow(radius: 10)
-                VStack {
-                    Image(uiImage: birthday_user_information.image!)
-                        .resizable()
-                           .aspectRatio(contentMode: .fill)
-                           .frame(width: 250.0, height: 250.0)
-                           .clipShape(Circle())
-                           .shadow(radius: 20)
-                    HStack{
-                        Spacer()
-                        Text("🎊")
-                        Text("🥳")
-                        Text("㊗️")
-                        Spacer()
-                        Text("🎊")
-                        Text("🥳")
-                        Text("㊗️")
-                        Spacer()
-                    }.font(.largeTitle).fontWeight(.black)
-                    Text("\(birthday_user_information.name)さんの詳細").font(.title).fontWeight(.black)
-                    Text("誕生日は\(birthday_user_information.year)年\(birthday_user_information.month)月\(birthday_user_information.day)日です。").font(.title2).fontWeight(.black)
-                    Text("和暦は\(birthday_user_information.japanese_calender)です。").font(.title2).fontWeight(.black)
-                    Text("\(birthday_user_information.name)さんが好きなことは").font(.title2).fontWeight(.black)
-                    VStack{
-                        Text(birthday_user_information.what_he_likes).font(.title).fontWeight(.black)
-                    }.padding()
+        NavigationView{
+            VStack{
+                ZStack {
+                    RoundedRectangle(cornerRadius: 100)
+                        .fill(Color.gray)
+                        .shadow(radius: 10)
+                    VStack {
+                        Image(uiImage: birthday_user_information.image!)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 250.0, height: 250.0)
+                            .clipShape(Circle())
+                            .shadow(radius: 20)
+                        HStack{
+                            Spacer()
+                            Text("🎊")
+                            Text("🥳")
+                            Text("㊗️")
+                            Spacer()
+                            Text("🎊")
+                            Text("🥳")
+                            Text("㊗️")
+                            Spacer()
+                        }.font(.largeTitle).fontWeight(.black)
+                        Text("\(birthday_user_information.name)さんの詳細").font(.title).fontWeight(.black)
+                        Text("誕生日は\(birthday_user_information.year)年\(birthday_user_information.month)月\(birthday_user_information.day)日です。").font(.title2).fontWeight(.black)
+                        Text("和暦は\(birthday_user_information.japanese_calender)です。").font(.title2).fontWeight(.black)
+                        Text("\(birthday_user_information.name)さんが好きなことは").font(.title2).fontWeight(.black)
+                        VStack{
+                            Text(birthday_user_information.what_he_likes).font(.title).fontWeight(.black)
+                        }.padding()
+                    }
+                    .foregroundColor(.yellow)
                 }
-                .foregroundColor(.yellow)
+                Spacer()
             }
-            Spacer()
-        }.onAppear{print(birthday_user_information.what_he_likes)}
+        }.navigationBarItems(leading: Button("✖️"){
+            presentationMode.wrappedValue.dismiss()
+        })
     }
 }
